f0ef9fdbbb33ad79e6b245f3ed9141611d4173e4
vignesh vasanth <vigneshvasanth.c@trustrace.com>
2025-06-22 07:31:05 +0530

    ng model is added

--- src/app/user-form/user-form.ts
+++ src/app/user-form/user-form.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+  ],
   templateUrl: './user-form.html',
-  styleUrl: './user-form.scss'
+  styleUrl: './user-form.scss',
 })
 export class UserForm {
-
+  userName = '';
 }
