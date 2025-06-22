6a00e23940b42ec139ab86692718e861d53d5795
vignesh vasanth <vigneshvasanth.c@trustrace.com>
2025-06-22 06:08:56 +0530

    page not found component is added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { TopNavBar } from './top-nav-bar/top-nav-bar';
+import { UserForm } from './user-form/user-form';
+import { ToDoList } from './to-do-list/to-do-list';
+import { Login } from './login/login';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', redirectTo: '', component: Home },
+  { path: 'top-nav-bar', component: TopNavBar },
+  { path: 'user-form', component: UserForm },
+  { path: 'to-do-list', component: ToDoList },
+  { path: 'login', component: Login },
+  { path: '**', component: PageNotFound },
+];
