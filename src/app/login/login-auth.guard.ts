0e09303e69ee465138583829d7fe1e524a8b29d8
vignesh vasanth <vigneshvasanth.c@trustrace.com>
2025-06-22 16:48:10 +0530

    auth guard issue is fixed

--- src/app/login/login-auth.guard.ts
+++ src/app/login/login-auth.guard.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class LoginAuthGuard implements CanActivate {
+  private platformId = inject(PLATFORM_ID);
+
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const token = localStorage.getItem('token');
-    if (token) {
-      return true;
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('token');
+      if (token) return true;
     }
+
     this.router.navigate(['/login']);
     return false;
   }
